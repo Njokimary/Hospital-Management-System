9417fbb6836b5db3cb256bba45539e40c8258e5a
Njokimary <marynjoki738@gmail.com>
2023-10-05 12:26:20 +0300

    feat:create a docs instance

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker, relationship
 from datetime import datetime
 
 
-db_url = "sqlite:///hospital.db"
+db_url = "sqlite:///hospitals.db"
 engine = create_engine(db_url)
 
 
@@ -49,6 +49,20 @@ session = Session()
 Base.metadata.create_all(engine)
 
 # create a patient instance
-new_patient = Patient(first_name='John', last_name='Doe', gender='Male', contact=1234567890, email='johndoe@gmail.com')
-session.add(new_patient)
+new_patient1 = Patient(first_name='John', last_name='Doe', gender='Male', contact=1234567890, email='johndoe@gmail.com')
+session.add(new_patient1)
+session.commit()
+
+new_patient2 = Patient(first_name='Mary', last_name='Brown', gender='Female', contact=2233002345, email='marybrown@gmail.com')
+session.add(new_patient2)
+session.commit()
+
+
+# create a doctor instance
+new_doctor1 = Doctor(first_name='Jane', last_name='Smith', specialty='Cardiology', department='Cardiology', phone=1234567890, email='janesmith@example.com')
+session.add(new_doctor1)
+session.commit()
+
+new_doctor2 = Doctor(first_name='James', last_name='Ice', specialty='Neurologist', department='Nerves', phone=222456900, email='jamesice@gmail.com')
+session.add(new_doctor2)
 session.commit()
